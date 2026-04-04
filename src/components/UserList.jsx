db9ed514a5a10c5dd7c6970b9b8181739114f314
aiethamdivyasri07 <divyasriaietham@gmail.com>
2026-04-04 11:35:39 +0530

    11 experiment

---
+++ src/components/UserList.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+
+function UserList() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        if (!res.ok) throw new Error("API Error");
+        return res.json();
+      })
+      .then((data) => {
+        setUsers(data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) return <p>Loading API users...</p>;
+  if (error) return <p>Error: {error}</p>;
+
+  return (
+    <div>
+      <h2>Users API</h2>
+      {users.map((user) => (
+        <div key={user.id}>
+          <p>{user.name}</p>
+          <p>{user.email}</p>
+          <p>{user.phone}</p>
+          <hr />
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default UserList;
